91c73ded523f99e3c92ab1c623d9ef8f19eebaf5
edwingarciagaitan82 <edwingarciagaitan82@gmail.com>
2024-09-03 19:58:05 -0500

    EDU ajuste puerto 3001

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
+const puerto = 3001
 app.get("/", (req,res) =>{
     res.send("Hola Mundo")
 } )
 app.get("/nombre", (req,res) =>{
     res.send("Talento Tech")
 })
-app.listen(3000, ()=>{ console.log("servidor express en puerto 3000")  } )
+app.listen(puerto, ()=>{ console.log("servidor express en puerto " , puerto)  } )
